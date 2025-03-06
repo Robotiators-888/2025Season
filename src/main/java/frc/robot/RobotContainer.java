7e699c3e0055e7e1b6fc00dd6734432f41a25e44
KevinH45 <timmyhwang127@gmail.com>
2025-03-06 13:52:06 -0500

    changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -582,33 +582,33 @@ public class RobotContainer {
                         }
                 }
 
-                // photonPoseOptional = photonVision.getCam2Pose();
+                photonPoseOptional = photonVision.getCam2Pose();
 
-                // if (photonPoseOptional.isPresent()) {
-                // Pose3d photonPose = photonPoseOptional.get().estimatedPose;
+                if (photonPoseOptional.isPresent()) {
+                Pose3d photonPose = photonPoseOptional.get().estimatedPose;
 
-                // if (photonPose.getX() >= 0 && photonPose.getX() <= Field.fieldLength &&
-                // photonPose.getY()
-                // >= 0
-                // && photonPose.getY() <= Field.fieldWidth && photonVision.getCam2BestTarget() !=
-                // null) {
+                if (photonPose.getX() >= 0 && photonPose.getX() <= Field.fieldLength &&
+                photonPose.getY()
+                >= 0
+                && photonPose.getY() <= Field.fieldWidth && photonVision.getCam2BestTarget() !=
+                null) {
 
-                // Pose2d closestTag = photonVision.at_field
-                // .getTagPose(photonVision.getCam2BestTarget().getFiducialId()).get().toPose2d();
-                // Translation2d translate =
-                // closestTag.minus(photonPose.toPose2d()).getTranslation();
+                Pose2d closestTag = photonVision.at_field
+                .getTagPose(photonVision.getCam2BestTarget().getFiducialId()).get().toPose2d();
+                Translation2d translate =
+                closestTag.minus(photonPose.toPose2d()).getTranslation();
 
-                // double distance = translate.getNorm();
-                // double xStddev = distance * 1.5;
-                // double yStddev = xStddev * 4;
-                // double rotStddev = Units.degreesToRadians(120.0);
-                // drivetrain.publisher4.set(photonPose.toPose2d());
-                // drivetrain.m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(xStddev,
-                // yStddev,
-                // rotStddev));
-                // drivetrain.addVisionMeasurement(photonPose.toPose2d(),
-                // photonPoseOptional.get().timestampSeconds);
-                // }
-                // }
+                double distance = translate.getNorm();
+                double xStddev = distance / 16.0;
+                double yStddev = xStddev;
+                double rotStddev = Units.degreesToRadians(120.0);
+                drivetrain.publisher4.set(photonPose.toPose2d());
+                drivetrain.m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(xStddev,
+                yStddev,
+                rotStddev));
+                drivetrain.addVisionMeasurement(photonPose.toPose2d(),
+                photonPoseOptional.get().timestampSeconds);
+                }
+                }
         }
 }
