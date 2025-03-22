ffe5c543d85ce87a749ebd745cf06f6b1d224480
ajassi9751 <ajassi9751@gmail.com>
2025-03-22 14:55:05 -0400

    Removed unused imports

--- src/main/java/frc/robot/commands/CMD_PathfindReefAlign.java
+++ src/main/java/frc/robot/commands/CMD_PathfindReefAlign.java
@@ -19,7 +19,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.SUB_Drivetrain;
 import frc.robot.subsystems.SUB_PhotonVision;
 
