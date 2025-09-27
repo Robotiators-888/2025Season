07f3bc28c6436cbcd4220c2f114688fe6910c706
Aikam <ajassi9751@gmail.com>
2025-09-27 11:29:42 -0400

    Commented out algae commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -391,10 +391,10 @@ public class RobotContainer {
                 // Driver2.y().onTrue(new InstantCommand(()->pivot.changeSetpoint(PivotConstants.kL4Setpoint)));
 
 
-                Driver2.povUp().onTrue(getAlgaeSetpointCommand());
-                Driver2.povDown().onTrue(getL2AlgaeSetpointCommand());
-                Driver2.povLeft().onTrue(getProcessorSetpointCommand());
-                Driver2.povRight().onTrue(getBargeScoringCommand());
+                // Driver2.povUp().onTrue(getAlgaeSetpointCommand());
+                // Driver2.povDown().onTrue(getL2AlgaeSetpointCommand());
+                // Driver2.povLeft().onTrue(getProcessorSetpointCommand());
+                // Driver2.povRight().onTrue(getBargeScoringCommand());
                 Driver2.leftBumper().whileTrue(new RunCommand(()->roller.setRollerOutput(-Roller.kIntakeSpeed, -Roller.kRollerHelperSpeed))).onFalse(new InstantCommand(()->roller.setRollerOutput(0.0, 0.0)));
 
                 Driver2.start().onTrue(new InstantCommand(
